9df245f70278348c495e900714fd3d7d26a2c3bd
BubnovaA <42323570+BubnovaA@users.noreply.github.com>
2023-08-31 18:47:57 +0300

    fix
    fix

--- monitoringApyTvl.js
+++ monitoringApyTvl.js
@@ -1,230 +1,296 @@
-import { ethers } from "ethers";
+import { ethers } from 'ethers'
 import {
-    ETH_NODE,
-    ARB_NODE,
-    threshold1,
-    threshold2,
-    calculateDeviationPercent,
-    getAndFormatDate,
-} from "./utils/utils.js";
-import { getLastData, writeAlertTs, getAlertsTS } from "./utils/database.js";
-import { sendMessageToDiscord, sendMessageToMessageBird } from "./utils/alert.js";
-import { strategies } from "./strategy_list/apyStrategy.js";
-import { strategiesTVL } from "./strategy_list/tvlStrategy.js";
-import { getPrice } from "./utils/price.js";
+  ETH_NODE,
+  ARB_NODE,
+  threshold1,
+  threshold2,
+  calculateDeviationPercent,
+  getAndFormatDate
+} from './utils/utils.js'
+import { getLastData, writeAlertTs, getAlertsTS } from './utils/database.js'
+import {
+  sendMessageToDiscord,
+  sendMessageToMessageBird
+} from './utils/alert.js'
+import { strategies } from './strategy_list/apyStrategy.js'
+import { strategiesTVL } from './strategy_list/tvlStrategy.js'
+import { getPrice } from './utils/price.js'
 
 const getApy = async (strategy, provider, prices) => {
-    const { strategy_id, strategy_addr, vault_addr, method, params, chain } = strategy;
-    const network = chain === 1 ? 'ETH' : 'ARB';
-    const stratProvider = provider[chain];
-    const [apy, tvl] = await method(stratProvider, ...params, prices);
-    return {
-        strategy_id,
-        strategy_addr,
-        vault_addr,
-        network,
-        apy,
-        tvl,
-        timestamp: Math.floor(Date.now() / 1000),
-        created_at: getAndFormatDate()
-    }
+  const { strategy_id, strategy_addr, vault_addr, method, params, chain } =
+    strategy
+  const network = chain === 1 ? 'ETH' : 'ARB'
+  const stratProvider = provider[chain]
+  const [apy, tvl] = await method(stratProvider, ...params, prices)
+  return {
+    strategy_id,
+    strategy_addr,
+    vault_addr,
+    network,
+    apy,
+    tvl,
+    timestamp: Math.floor(Date.now() / 1000),
+    created_at: getAndFormatDate()
+  }
 }
-
-
 
 const saveToDB = async (data, fn) => {
-    let result = true;
-    try {
-        for (const record of data) {
-            const insert = await fn(record, db);
-            result = result * insert;
-        }
+  let result = true
+  try {
+    for (const record of data) {
+      const insert = await fn(record, db)
+      result = result * insert
     }
-    catch (err) {
-        console.error('Error_:', err.message);
-        result = false;
-    }
-    finally {
-        return result;
-    }
+  } catch (err) {
+    console.error('Error_:', err.message)
+    result = false
+  } finally {
+    return result
+  }
 }
 
-
-const saveApyStatsToPG = async (pgClient,  data) => {
-    try {
-        const values = data.map(item =>
-            `('${item.strategy_id}', ${item.tvl}, ${item.apy}, ${item.timestamp})`
-        ).join(', ');
-        const queryText = `INSERT INTO alerts_apy_stats (strategy_id, tvl, apy, timestamp) VALUES ${values}`;
-        await pgClient.query(queryText);
-    } catch (error) {
-        console.error('Error in transaction:', error);
-    }
-
+const saveApyStatsToPG = async (pgClient, data) => {
+  try {
+    const values = data
+      .map(
+        item =>
+          `('${item.strategy_id}', ${item.tvl}, ${item.apy}, ${item.timestamp})`
+      )
+      .join(', ')
+    const queryText = `INSERT INTO alerts_apy_stats (strategy_id, tvl, apy, timestamp) VALUES ${values}`
+    await pgClient.query(queryText)
+  } catch (error) {
+    console.error('Error in transaction:', error)
+  }
 }
 
-const saveTvlStatsToPG = async (pgClient,  data) => {
-    try {
-        const values = data.map(item =>
-            `('${item.strategy_id}', ${item.tvl}, ${item.timestamp})`
-        ).join(', ');
-        const queryText = `INSERT INTO alerts_tvl_stats (strategy_id, tvl, timestamp) VALUES ${values}`;
-        await pgClient.query(queryText);
-    } catch (error) {
-        console.error('Error in transaction:', error);
-    }
-
+const saveTvlStatsToPG = async (pgClient, data) => {
+  try {
+    const values = data
+      .map(item => `('${item.strategy_id}', ${item.tvl}, ${item.timestamp})`)
+      .join(', ')
+    const queryText = `INSERT INTO alerts_tvl_stats (strategy_id, tvl, timestamp) VALUES ${values}`
+    await pgClient.query(queryText)
+  } catch (error) {
+    console.error('Error in transaction:', error)
+  }
 }
 
 const saveToPG = async (pgClient, data) => {
-    try {
-        const values = data.map(item =>
-            `('${item.strategy_addr}', '${item.vault_addr}', '${item.network}', ${item.apy}, '${item.created_at}')`
-        ).join(', ');
-        const queryText = `INSERT INTO strategies_apy (strategy_addr, vault_addr, network, apy, created_at ) VALUES ${values}`;
-        await pgClient.query(queryText);
-    } catch (error) {
-        console.error('Error in transaction:', error);
-    }
-
+  try {
+    const values = data
+      .map(
+        item =>
+          `('${item.strategy_addr}', '${item.vault_addr}', '${item.network}', ${item.apy}, '${item.created_at}')`
+      )
+      .join(', ')
+    const queryText = `INSERT INTO strategies_apy (strategy_addr, vault_addr, network, apy, created_at ) VALUES ${values}`
+    await pgClient.query(queryText)
+  } catch (error) {
+    console.error('Error in transaction:', error)
+  }
 }
-
 
 const loadAPY = async (provider, pgClient) => {
-    const prices = await getPrice(['balancer',
-        'aura-finance',
-        'aura-bal',
-        'gmd-protocol',
-        'gains-network',
-        'weth',
-        'gmx',
-        'ethereum',
-        'rocket-pool-eth',
-        'curve-dao-token',
-        'convex-fxs',
-        'convex-crv',
-        'convex-finance',
-        'frax-share',
-    ]);
-    let result = false;
-    if (prices && strategies?.length) {
-        const data = await Promise.all(strategies.map(strategy => getApy(strategy, provider, prices)));
-        const filteredData = data.filter(elem => elem != undefined);
-        if (filteredData.length > 0) {
-            result = await saveApyStatsToPG(pgClient, filteredData);
+  const prices = await getPrice([
+    'balancer',
+    'aura-finance',
+    'aura-bal',
+    'gmd-protocol',
+    'gains-network',
+    'weth',
+    'gmx',
+    'ethereum',
+    'rocket-pool-eth',
+    'curve-dao-token',
+    'convex-fxs',
+    'convex-crv',
+    'convex-finance',
+    'frax-share'
+  ])
+  let result = false
+  if (prices && strategies?.length) {
+    const data = await Promise.all(
+      strategies.map(strategy => getApy(strategy, provider, prices))
+    )
+    const filteredData = data.filter(elem => elem != undefined)
+    if (filteredData.length > 0) {
+      result = await saveApyStatsToPG(pgClient, filteredData)
 
-            //  console.log(filteredData);
-           await saveToPG(pgClient, filteredData);
-        }
+      //  console.log(filteredData);
+      await saveToPG(pgClient, filteredData)
     }
-    else {
-        console.log("price error");
-        return false;
-    }
-    return result;
+  } else {
+    console.log('price error')
+    return false
+  }
+  return result
 }
-
 
 const checkApyTvl = async (pgClient, alertsTS) => {
-    const oneDay = 24 * 60 * 60;
-    const oneWeek = 7 * oneDay;
-    const now = Math.floor(Date.now() / 1000);
-    const dateDayAgo = now - oneDay;
-    const date7DayAgo = now - oneWeek;
-    const lastData = await getLastData(pgClient, dateDayAgo, date7DayAgo);
-    await Promise.all(lastData.map(async (data) => {
-        Object.entries(data).forEach(([key, valueArray]) => {
-            valueArray.map(async row => {
-                return await checkPercent(pgClient, row, key, alertsTS)
-            })
-        });
-    }));
+  const oneDay = 24 * 60 * 60
+  const oneWeek = 7 * oneDay
+  const now = Math.floor(Date.now() / 1000)
+  const dateDayAgo = now - oneDay
+  const date7DayAgo = now - oneWeek
+  const lastData = await getLastData(pgClient, dateDayAgo, date7DayAgo)
+  await Promise.all(
+    lastData.map(async data => {
+      Object.entries(data).forEach(([key, valueArray]) => {
+        valueArray.map(async row => {
+          return await checkPercent(pgClient, row, key, alertsTS)
+        })
+      })
+    })
+  )
 }
 
-const sendAlert = async (pgClient, strategy_id, threshold, key, currValue, avgValue, alertsTS, period, call) => {
-    const messageApy = `Стратегия ${strategy_id}: превышен порог ${threshold}% отклонения текущего значения ${key.toUpperCase()} от среднего ${key.toUpperCase()} за ${period}. Текущее значение: ${currValue.toFixed(2)}% (Среднее значение ${avgValue.toFixed(2)}%)`;
-    const messageTvl = `Среднее значение total staked ${strategy_id} за ${period} снизилось более, чем на ${threshold}%. Текущий totalSupply LP: ${currValue.toFixed(2)}. Среднее значение: ${avgValue.toFixed(2)}`;
-    const message = key.toUpperCase()==="TVL"? messageTvl : messageApy;
-    const lastAlertTS = alertsTS[strategy_id][key] ? alertsTS[strategy_id][key] : 0;
-    const now = Math.floor(Date.now() / 1000);
-    const diff = now - lastAlertTS;
-    const timeDiff = (3600 * 12);
+const sendAlert = async (
+  pgClient,
+  strategy_id,
+  threshold,
+  key,
+  currValue,
+  avgValue,
+  alertsTS,
+  period,
+  call
+) => {
+  if (currValue && currValue > 0) {
+    const messageApy = `Стратегия ${strategy_id}: превышен порог ${threshold}% отклонения текущего значения ${key.toUpperCase()} от среднего ${key.toUpperCase()} за ${period}. Текущее значение: ${currValue.toFixed(
+      2
+    )}% (Среднее значение ${avgValue.toFixed(2)}%)`
+    const messageTvl = `Среднее значение total staked ${strategy_id} за ${period} снизилось более, чем на ${threshold}%. Текущий totalSupply LP: ${currValue.toFixed(
+      2
+    )}. Среднее значение: ${avgValue.toFixed(2)}`
+    const message = key.toUpperCase() === 'TVL' ? messageTvl : messageApy
+    const lastAlertTS = alertsTS[strategy_id][key]
+      ? alertsTS[strategy_id][key]
+      : 0
+    const now = Math.floor(Date.now() / 1000)
+    const diff = now - lastAlertTS
+    const timeDiff = 3600 * 12
     if (diff > timeDiff) {
-        const newRow = lastAlertTS === 0 ? true : false;
-        await writeAlertTs(pgClient, strategy_id, key, now, newRow);
-        console.log(message);
-        await sendMessageToDiscord(message);
-        if (call) {
-           await sendMessageToMessageBird(message);
-        }
+      const newRow = lastAlertTS === 0 ? true : false
+      await writeAlertTs(pgClient, strategy_id, key, now, newRow)
+      console.log(message)
+      await sendMessageToDiscord(message)
+      if (call) {
+        await sendMessageToMessageBird(message)
+      }
     }
+  }
 }
-
 
 const checkPercent = async (pgClient, strategy, key, alertsTS) => {
-    const { strategy_id, last_value, avg_value_daily, avg_value_7_days } = strategy;
+  const { strategy_id, last_value, avg_value_daily, avg_value_7_days } =
+    strategy
 
-    const deviationPercentDaily = calculateDeviationPercent(last_value, avg_value_daily);
-    const deviationPercent7Days = calculateDeviationPercent(last_value, avg_value_7_days);
-    if (deviationPercentDaily > threshold1 && deviationPercentDaily < threshold2) {
-        await sendAlert(pgClient, strategy_id, threshold1, key, last_value, avg_value_daily, alertsTS, 'день', false);
-    }
-    else 
-    if (deviationPercent7Days > threshold1 && deviationPercent7Days < threshold2) {
-        await sendAlert(pgClient, strategy_id, threshold1, key, last_value, avg_value_7_days, alertsTS, 'неделю', false);
-    }
-    if (deviationPercentDaily > threshold2) {
-        await sendAlert(strategy_id, threshold2, key, last_value, avg_value_daily, alertsTS, 'день', true);
-    }
-    else
-    if (deviationPercent7Days > threshold2) {
-        await sendAlert(strategy_id, threshold2, key, last_value, avg_value_7_days, alertsTS, 'неделю', true);
-    }
+  const deviationPercentDaily = calculateDeviationPercent(
+    last_value,
+    avg_value_daily
+  )
+  const deviationPercent7Days = calculateDeviationPercent(
+    last_value,
+    avg_value_7_days
+  )
+  if (
+    deviationPercentDaily > threshold1 &&
+    deviationPercentDaily < threshold2
+  ) {
+    await sendAlert(
+      pgClient,
+      strategy_id,
+      threshold1,
+      key,
+      last_value,
+      avg_value_daily,
+      alertsTS,
+      'день',
+      false
+    )
+  } else if (
+    deviationPercent7Days > threshold1 &&
+    deviationPercent7Days < threshold2
+  ) {
+    await sendAlert(
+      pgClient,
+      strategy_id,
+      threshold1,
+      key,
+      last_value,
+      avg_value_7_days,
+      alertsTS,
+      'неделю',
+      false
+    )
+  }
+  if (deviationPercentDaily > threshold2) {
+    await sendAlert(
+      strategy_id,
+      threshold2,
+      key,
+      last_value,
+      avg_value_daily,
+      alertsTS,
+      'день',
+      true
+    )
+  } else if (deviationPercent7Days > threshold2) {
+    await sendAlert(
+      strategy_id,
+      threshold2,
+      key,
+      last_value,
+      avg_value_7_days,
+      alertsTS,
+      'неделю',
+      true
+    )
+  }
 }
 
-
 const getTvl = async (strategy, provider) => {
-    const { strategy_id, contractAddress, abi, method, params, chain } = strategy;
-    const stratProvider = provider[chain];
-    const contract = new ethers.Contract(contractAddress, abi, stratProvider);
-    let result;
-    if (params && params.length > 0) {
-        result = await contract[method](...params);
-    } else {
-        result = await contract[method]();
-    }
-    const tvl = ethers.formatEther(result);
-    return { strategy_id, tvl, timestamp: Math.floor(Date.now() / 1000) };
+  const { strategy_id, contractAddress, abi, method, params, chain } = strategy
+  const stratProvider = provider[chain]
+  const contract = new ethers.Contract(contractAddress, abi, stratProvider)
+  let result
+  if (params && params.length > 0) {
+    result = await contract[method](...params)
+  } else {
+    result = await contract[method]()
+  }
+  const tvl = ethers.formatEther(result)
+  return { strategy_id, tvl, timestamp: Math.floor(Date.now() / 1000) }
 }
 
 const loadTVL = async (provider, pgClient) => {
-    let result = false;
-    const data = await Promise.all(strategiesTVL.map(strategy => getTvl(strategy, provider)));
-    const filteredData = data.filter(elem => elem != undefined);
-    if (filteredData.length > 0) {
-        result = await saveTvlStatsToPG(pgClient, filteredData);
-    }
-    return result;
+  let result = false
+  const data = await Promise.all(
+    strategiesTVL.map(strategy => getTvl(strategy, provider))
+  )
+  const filteredData = data.filter(elem => elem != undefined)
+  if (filteredData.length > 0) {
+    result = await saveTvlStatsToPG(pgClient, filteredData)
+  }
+  return result
 }
 
-
-export const apyLoadCheck = async (pgClient) => {
-    try {
-        const ethProvider = new ethers.JsonRpcProvider(ETH_NODE);
-        const arbProvider = new ethers.JsonRpcProvider(ARB_NODE);
-        const provider = {
-            1: ethProvider,
-            42161: arbProvider,
-        };
-        const resultLoad = await loadAPY(provider, pgClient);
-        const resultLoadTvl = await loadTVL(provider, pgClient);
-        const alertsTS = await getAlertsTS(pgClient);
-        const resultCheck = await checkApyTvl(pgClient, alertsTS);
-        return resultLoad;
+export const apyLoadCheck = async pgClient => {
+  try {
+    const ethProvider = new ethers.JsonRpcProvider(ETH_NODE)
+    const arbProvider = new ethers.JsonRpcProvider(ARB_NODE)
+    const provider = {
+      1: ethProvider,
+      42161: arbProvider
     }
-    catch (error) {
-        console.log(error);
-        return false;
-    }
+    const resultLoad = await loadAPY(provider, pgClient)
+    const resultLoadTvl = await loadTVL(provider, pgClient)
+    const alertsTS = await getAlertsTS(pgClient)
+    const resultCheck = await checkApyTvl(pgClient, alertsTS)
+    return resultLoad
+  } catch (error) {
+    console.log(error)
+    return false
+  }
 }
-
--- monitoringPI.js
+++ monitoringPI.js
@@ -1,66 +1,108 @@
 import { swapPrice } from './strategy_list/priceImpact.js'
-import { ethers } from 'ethers';
-import { ETH_NODE, ARB_NODE } from "./utils/utils.js";
+import { ethers } from 'ethers'
+import { ETH_NODE, ARB_NODE } from './utils/utils.js'
 
 const BASE_URL = 'https://api.1inch.dev/swap/v5.2/'
 const apiKey = 'JEp4qoSseECYg575GwDDjLfHJJ4h9Ogz'
 
-const calculatePriceImpact = (smallAmountPrice, largeAmountPrice, smallAmount, largeAmount) => {
-  const price0 = smallAmountPrice/smallAmount; 
-  const price1 = largeAmountPrice/largeAmount;
-  const impact = ((price1 - price0) / price0) * 100;
-  return impact;
-};
-
-const fetchPrice = async (fromToken, toToken, amount, chain) => {
-  const response = await fetch(
-    `${BASE_URL}${chain}/quote?src=${fromToken}&dst=${toToken}&amount=${amount}`,
-    {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: `Bearer ${apiKey}`
-      }
-    }
-  );
-  const data = await response.json();
-  return parseFloat(data.toAmount);
-  
-};
-
-
-const checkPI = async (token, provider) => {
-  const { tokenAName, fromToken, tokenBName, toToken, chain, amount1, amount0 } = token
-
-  const smallAmountPrice = await fetchPrice(fromToken, toToken, amount0, chain);
-  await sleep(2000);
-  const largeAmountPrice = await fetchPrice(fromToken, toToken, amount1, chain);
-  const impact = calculatePriceImpact(smallAmountPrice, largeAmountPrice, amount0, amount1);
-  
-  if (impact > 3) {
-    console.log(`Warning: Price Impact for swapping ${tokenAName} to ${tokenBName} is more than 3%. Actual impact: ${impact.toFixed(4)}%`);
-    return false;  // Indicate that this token pair has high price impact
+const calculatePriceImpact = (
+  smallAmountPrice,
+  largeAmountPrice,
+  smallAmount,
+  largeAmount
+) => {
+  if (smallAmountPrice && smallAmountPrice > 0 && largeAmountPrice && largeAmountPrice > 0) {
+    const price0 = smallAmountPrice / parseInt(smallAmount)
+    const price1 = largeAmountPrice / parseInt(largeAmount)
+    const impact = ((price1 - price0) / price0) * 100
+    return impact
   } else {
-    console.log(`Price Impact for swapping ${tokenAName} to ${tokenBName} is ${impact.toFixed(4)}%`);
-    return true;  // Indicate that this token pair has acceptable price impact
+    return 0
   }
 }
 
-
-async function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
+const fetchPrice = async (fromToken, toToken, amount, chain) => {
+  try {
+    const response = await fetch(
+      `${BASE_URL}${chain}/quote?src=${fromToken}&dst=${toToken}&amount=${amount}`,
+      {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: `Bearer ${apiKey}`
+        }
+      }
+    )
+    const data = await response.json()
+    return parseFloat(data.toAmount)
+  } catch {
+    console.log(error)
+    return 0
+  }
 }
 
+const checkPI = async (token, provider) => {
+  try {
+    const {
+      tokenAName,
+      fromToken,
+      tokenBName,
+      toToken,
+      chain,
+      amount1,
+      amount0
+    } = token
 
-const loadAllPI = async (provider) => {
+    const smallAmountPrice = await fetchPrice(
+      fromToken,
+      toToken,
+      amount0,
+      chain
+    )
+    await sleep(2000)
+    const largeAmountPrice = await fetchPrice(
+      fromToken,
+      toToken,
+      amount1,
+      chain
+    )
+    const impact = calculatePriceImpact(
+      smallAmountPrice,
+      largeAmountPrice,
+      amount0,
+      amount1
+    )
 
+    if (impact > 3) {
+      console.log(
+        `Warning: Price Impact for swapping ${tokenAName} to ${tokenBName} is more than 3%. Actual impact: ${impact.toFixed(
+          4
+        )}%`
+      )
+      return false // Indicate that this token pair has high price impact
+    } else {
+      console.log(
+        `Price Impact for swapping ${tokenAName} to ${tokenBName} is ${impact.toFixed(
+          4
+        )}%`
+      )
+      return true // Indicate that this token pair has acceptable price impact
+    }
+  } catch {
+    console.log(error)
+  }
+}
+
+async function sleep (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+const loadAllPI = async provider => {
   if (swapPrice?.length) {
     for (const item of swapPrice) {
-      await checkPI(item, provider);
-      await sleep(2000);
+      await checkPI(item, provider)
+      await sleep(2000)
     }
-    
-    
   } else {
     return false
   }
@@ -77,7 +119,7 @@ export const piCheck = async () => {
     const resultCheck = await loadAllPI(provider)
     return resultCheck
   } catch (error) {
-    console.log(error);
+    console.log(error)
     return false
   }
 }
